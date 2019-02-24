25cc8093fc54c2f2539388cd69fba7ec3f638d14
Jisoo Lee <traveler279c@gmail.com>
2019-02-25 01:13:36 +0900

    Implemented a function to store user name

--- greeting.js
+++ greeting.js
@@ -7,18 +7,36 @@ var greeting = document.querySelector('.js-greetings');
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
-// 필요한 기능(요구사항)을 구현한다. 어떨 때 어떤 함수를 호출할 것인지 물고 물리는 관계를 이해한다.
+// 필요한 기능(요구사항)을 구현한다. 어떨 때 어떤 함수를 호출할 것인지 물고 물리는 관계를 이해한다. 어디에 어떤 이벤트가 구현되어야 하는지를 파악한다.
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
 
+function handleSubmit(event) {
+    event.preventDefault();
+    var currentValue = input.value;
+
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function saveName(name) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener('submit', handleSubmit);
+}
+
+
 // 필요한 기능(요구사항)을 구현한다.
 function loadName() {
     var currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
-
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
