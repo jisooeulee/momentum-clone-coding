6669e9153acdf1846c527ed189553644a3abde90
Jisoo Lee <traveler279c@gmail.com>
2019-03-17 01:16:38 +0900

    Added git ignore

--- weather.js
+++ weather.js
@@ -35,19 +35,16 @@ function handleGeoFailure() {
 }
 
 // 좌표를 요청
-function askForCoords() {
-    // DOM APIs가 제공하는 navigator API를 이용한다.
-    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoFailure); // .getCurrentPosition()의 첫번째 인자 : 좌표를 가져오는데 성공했을 때를 처리하는 함수.
-}
-
 function loadWeather() {
     var currentCoords = localStorage.getItem("coords");
     if (currentCoords !== null) {
         const parsedCoords = JSON.parse(currentCoords);
         getWeather(parsedCoords);
+
         return;
     } else {
-        navigator.geolocation.getCurrentPosition(
+        // DOM APIs가 제공하는 navigator API를 이용한다.
+        navigator.geolocation.getCurrentPosition( // .getCurrentPosition()의 첫번째 인자 : 좌표를 가져오는데 성공했을 때를 처리하는 함수.
             handleGeoSuccess,
             handleGeoFailure
         );
